750dc611f1217822084b0bdc48f1e05962970c9c
nulgaath <74437058+nulgaath@users.noreply.github.com>
2023-12-13 01:19:50 -0500

    added shrink effect

--- Assets/[Scripts]/Platform/MovingPlatformController.cs
+++ Assets/[Scripts]/Platform/MovingPlatformController.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class MovingPlatformController : MonoBehaviour
 {
+    public enum PlatformDirection
+    {
+        HORIZONTAL,
+        VERTICAL,
+        DIAGONAL_UP,
+        DIAGONAL_DOWN,
+        CUSTOM
+    }
+
     public PlatformDirection direction;
 
-    [Header("Movement Properties")] 
+    [Header("Movement Properties")]
     [Range(1.0f, 20.0f)]
     public float horizontalDistance = 8.0f;
-    [Range(1.0f, 20.0f)] 
+    [Range(1.0f, 20.0f)]
     public float horizontalSpeed = 3.0f;
     [Range(1.0f, 20.0f)]
     public float verticalDistance = 8.0f;
@@ -19,7 +27,14 @@ public class MovingPlatformController : MonoBehaviour
     [Range(0.001f, 0.1f)]
     public float customSpeedFactor = 0.02f;
 
-    [Header("Platform Path Points")] 
+    [Header("Shrink Properties")]
+    [Range(0.1f, 1.0f)]
+    public float minScale = 0.5f;
+    [Range(1.0f, 2.0f)]
+    public float maxScale = 1.5f;
+    public float shrinkSpeed = 1.0f;
+
+    [Header("Platform Path Points")]
     public List<Transform> pathPoints;
 
     private Vector2 startPoint;
@@ -34,9 +49,8 @@ public class MovingPlatformController : MonoBehaviour
         timer = 0.0f;
         currentPointIndex = 0;
         startPoint = transform.position;
-        pathList = new List<Vector2>(); // create an empty container of Vector2s
+        pathList = new List<Vector2>();
 
-        // copy each pointPoint Transform to the pathList and add startPoint
         for (int i = 0; i < pathPoints.Count; i++)
         {
             Vector2 point = new Vector2(pathPoints[i].localPosition.x + startPoint.x,
@@ -85,7 +99,7 @@ public class MovingPlatformController : MonoBehaviour
         {
             case PlatformDirection.HORIZONTAL:
                 transform.position = new Vector2(
-        Mathf.PingPong(horizontalSpeed * Time.time, horizontalDistance) + startPoint.x, startPoint.y);
+                    Mathf.PingPong(horizontalSpeed * Time.time, horizontalDistance) + startPoint.x, startPoint.y);
                 break;
             case PlatformDirection.VERTICAL:
                 transform.position = new Vector2(startPoint.x,
@@ -93,7 +107,7 @@ public class MovingPlatformController : MonoBehaviour
                 break;
             case PlatformDirection.DIAGONAL_UP:
                 transform.position = new Vector2(
-                    Mathf.PingPong(horizontalSpeed * Time.time, horizontalDistance) + startPoint.x, 
+                    Mathf.PingPong(horizontalSpeed * Time.time, horizontalDistance) + startPoint.x,
                     Mathf.PingPong(verticalSpeed * Time.time, verticalDistance) + startPoint.y);
                 break;
             case PlatformDirection.DIAGONAL_DOWN:
@@ -102,8 +116,14 @@ public class MovingPlatformController : MonoBehaviour
                     startPoint.y - Mathf.PingPong(verticalSpeed * Time.time, verticalDistance));
                 break;
             case PlatformDirection.CUSTOM:
+                // Shrink effect
+                float scaleFactor = Mathf.Lerp(maxScale, minScale, timer);
+                transform.localScale = new Vector3(scaleFactor, scaleFactor, 1f);
+
+                // Movement
                 transform.position = Vector2.Lerp(startPoint, destinationPoint, timer);
                 break;
         }
     }
 }
+
